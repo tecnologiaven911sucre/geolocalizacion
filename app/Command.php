ed1ce39ec92a2dd3e22b394e831a4cdef0a1a035
tecnologia ven911 sucre <tecnologia.ven911.cumana@gmail.com>
2020-06-26 11:12:42 -0430

    add modelo y controlador de centro de comando mas alimentando los modelos de informacion

---
+++ app/Command.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Command extends Model
+{
+    public function drawer(){
+        return $this->hasMany('App\Drawer');
+    }
+}
